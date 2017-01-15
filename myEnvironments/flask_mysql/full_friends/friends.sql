d24a777e991c5e4b687b8edab469cc7271f08863
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-01-15 02:35:40 -0500

    Start full friends project

---
+++ myEnvironments/flask_mysql/full_friends/friends.sql
@@ -0,0 +1,33 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `friendsdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `friendsdb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`friends`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `friendsdb`.`friends` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ myEnvironments/flask_mysql/full_friends/server.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+from livereload import Server
+import re
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'ThisIsSecret'
+server = Server(app.wsgi_app)
+mysql = MySQLConnector(app, 'friendsdb')
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
+@app.route('/')
+def index():
+	query = "SELECT id, first_name, last_name, email, DATE_FORMAT(created_at, '%m/%e/%Y %l:%i%p')as created_at, DATE_FORMAT(created_at, '%m/%e/%Y %l:%i%p')as updated_at FROM friends"
+	friends = mysql.query_db(query)
+	print friends
+	return render_template("index.html", friends=friends)
+
+
+@app.route('/emails')
+def success():
+	query = "SELECT id, email, DATE_FORMAT(created_at, '%m/%e/%Y %l:%i%p')as created_at FROM emails"
+	emails = mysql.query_db(query)
+	print emails
+	return render_template('success.html', emails=emails)
+
+
+@app.route('/process', methods=['POST'])
+def submit():
+	if len(request.form['email']) < 1:
+		flash('You\'ve gotta type something man!')
+		return redirect('/')
+	elif not EMAIL_REGEX.match(request.form['email']):
+		flash('You\'ve submitted an incorrect email. Please, try again')
+		flash(request.form['email'])
+		return redirect('/')
+	else:
+		flash('The email address you entered (' + request.form['email'] + ') is a VALID email address! Thank you!')
+		query = "INSERT INTO emails (email, created_at, updated_at) VALUES (:email, NOW(), NOW())"
+		data = {
+			'email': request.form['email']
+		}
+		mysql.query_db(query, data)
+		return redirect('/emails')
+
+
+@app.route('/delete/<email_id>', methods=['POST', 'GET', 'DELETE'])
+def delete(email_id):
+	print email_id
+	query = "DELETE FROM emails WHERE id = :id"
+	data = {'id': email_id}
+	mysql.query_db(query, data)
+	return redirect('/emails')
+
+
+server.watch('./')
+server.serve(port=5500, host='127.0.1.1')
