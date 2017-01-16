456ba854c84507db8d8b8da71238248fe6eb8277
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-01-16 02:27:05 -0500

    Make changes to directory structure

--- myEnvironments/flask_mysql/friends_example/server.py
+++ myEnvironments/flask_mysql/friends_example/server.py
@@ -1,60 +1,55 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 from livereload import Server
+import re
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'ThisIsSecret'
 server = Server(app.wsgi_app)
-mysql = MySQLConnector(app, 'friendsdb')
+mysql = MySQLConnector(app, 'emailsdb')
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
 @app.route('/')
 def index():
-	friends = mysql.query_db("SELECT * FROM friends")
-	print friends
-	return render_template('index.html', friends=friends)
+	return render_template('index.html')
 
 
-@app.route('/friends/<friend_id>')
-def show(friend_id):
-	query = "Select * FROM friends WHERE id = :specific_id"
-	data = {'specific_id': friend_id}
-	friend = mysql.query_db(query, data)
-	return render_template('index.html', friends=friend)
+@app.route('/emails')
+def success():
+	query = "SELECT id, email, DATE_FORMAT(created_at, '%m/%e/%Y %l:%i%p')as created_at FROM emails"
+	emails = mysql.query_db(query)
+	print emails
+	return render_template('success.html', emails=emails)
 
 
-@app.route('/friends', methods=['POST'])
-def create():
-	query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
-	data = {
-		'first_name': request.form['first_name'],
-		'last_name': request.form['last_name'],
-		'occupation': request.form['occupation']
-	}
+@app.route('/process', methods=['POST'])
+def submit():
+	if len(request.form['email']) < 1:
+		flash('You\'ve gotta type something man!')
+		return redirect('/')
+	elif not EMAIL_REGEX.match(request.form['email']):
+		flash('You\'ve submitted an incorrect email. Please, try again')
+		flash(request.form['email'])
+		return redirect('/')
+	else:
+		flash('The email address you entered (' + request.form['email'] + ') is a VALID email address! Thank you!')
+		query = "INSERT INTO emails (email, created_at, updated_at) VALUES (:email, NOW(), NOW())"
+		data = {
+			'email': request.form['email']
+		}
+		mysql.query_db(query, data)
+		return redirect('/emails')
+
+
+@app.route('/delete/<email_id>', methods=['POST', 'GET', 'DELETE'])
+def delete(email_id):
+	print email_id
+	query = "DELETE FROM emails WHERE id = :id"
+	data = {'id': email_id}
 	mysql.query_db(query, data)
-	return redirect('/')
-
-
-@app.route('/update_friend/<friend_id>', methods=['POST'])
-def update(friend_id):
-	query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation WHERE id = :id"
-	data = {
-		'first_name': request.form['first_name'],
-		'last_name': request.form['last_name'],
-		'occupation': request.form['occupation'],
-		'id': friend_id
-	}
-	mysql.query_db(query, data)
-	return redirect('/')
-
-
-@app.route('/remove_friend/<friend_id>', methods=['POST'])
-def delete(friend_id):
-	query = "DELETE FROM friends WHERE id = :id"
-	data = {'id': friend_id}
-	mysql.query_db(query, data)
-	return redirect('/')
+	return redirect('/emails')
 
 
 server.watch('./')
